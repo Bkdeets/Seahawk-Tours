475a40422491d42635f9f180603116bf7f0eedc7
Britton <brittonkdeets@gmail.com>
2018-10-11 14:29:22 -0400

    1. Created DisplayActivity 2. Setup activity_main.xml with basic vertical linear view with buttons 3. Added building names to strings.xml

---
+++ app/src/main/java/edu/uncw/seahawktours/DisplayActivity.java
@@ -0,0 +1,4 @@
+package edu.uncw.seahawktours;
+
+public class DisplayActivity {
+}
--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uncw.seahawktours;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickDisplay(View view) {
+        //Launch the DisplayActivity with building data
+
     }
 }
