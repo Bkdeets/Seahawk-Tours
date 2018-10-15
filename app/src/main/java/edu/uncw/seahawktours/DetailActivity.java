ded1986ef1d55526dd8acf61c92b272cad164a1c
lisantimark <lisantimark@gmail.com>
2018-10-15 16:44:07 -0400

    Haven't put captions under images Need to test rotating devices

--- app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
@@ -3,31 +3,54 @@ package edu.uncw.seahawktours;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DetailActivity extends Activity {
+    String BuildingName;
+    ImageView buildingPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Creates off saved state?
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
-        String BuildingName = intent.getExtras().getString("building_name"); //Pull button text
+
+        buildingPhoto = findViewById(R.id.building_image);
+        BuildingName = intent.getExtras().getString("building_name"); //Pull button text
         TextView title = findViewById(R.id.building_name); //Define text view from xml
-        title.setText(BuildingName); //Set button text to textview
+        title.setText(BuildingName); //Set selection text to textview
 
         //Fill building info corresponding to title
+        //Each if statement assigns correct strings and image src depending on selection
+
         if (BuildingName.equals(getResources().getString(R.string.dobo))) {
             TextView info = findViewById(R.id.building_info);
             info.setText(R.string.dobo_info);
+            int image = R.drawable.dobo;
+            String description = "Dobo Hall Image";
+            buildingPhoto.setImageResource(image);
+            buildingPhoto.setContentDescription(description);
         }
+
         if (BuildingName.equals(getResources().getString(R.string.cis))) {
             TextView info = findViewById(R.id.building_info);
             info.setText(R.string.cis_info);
+            int image = R.drawable.cis;
+            String description = "CIS Building Image";
+            buildingPhoto.setImageResource(image);
+            buildingPhoto.setContentDescription(description);
         }
-        if (BuildingName.equals(getResources().getString(R.string.csb))) {
+
+        if (BuildingName.equals(getResources().getString(R.string.depaolo))) {
             TextView info = findViewById(R.id.building_info);
-            info.setText(R.string.csb_info);
+            info.setText(R.string.depaolo_info);
+            int image = R.drawable.depaolo;
+            String description = "Depaolo Hall Image";
+            buildingPhoto.setImageResource(image);
+            buildingPhoto.setContentDescription(description);
         }
     }
 }
--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -4,22 +4,26 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
+
+    private Spinner mainSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainSpinner = findViewById(R.id.main_spinner);
     }
 
     public void onClickDisplay(View view) {
         //Launch the DisplayActivity with building data
-        Button button = (Button)view;
-        String buttonText = button.getText().toString(); //get text from button pushed
+        String spinnerText = mainSpinner.getSelectedItem().toString(); //get text from button pushed
         Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra("building_name", buttonText);  //pass button text to new intent
+        intent.putExtra("building_name", spinnerText);  //pass button text to new intent
         startActivity(intent);
     }
 }
