040347e9e7ebbc1e2907aeda9836457a4d2a8f0c
lisantimark <lisantimark@gmail.com>
2018-10-15 07:52:07 -0400

    Basic logic setting up intent

--- app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/DetailActivity.java
@@ -1,7 +1,9 @@
 package edu.uncw.seahawktours;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailActivity extends Activity {
 
@@ -9,5 +11,23 @@ public class DetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Intent intent = getIntent();
+        String BuildingName = intent.getExtras().getString("building_name"); //Pull button text
+        TextView title = findViewById(R.id.building_name); //Define text view from xml
+        title.setText(BuildingName); //Set button text to textview
+
+        //Fill building info corresponding to title
+        if (BuildingName.equals(getResources().getString(R.string.dobo))) {
+            TextView info = findViewById(R.id.building_info);
+            info.setText(R.string.dobo_info);
+        }
+        if (BuildingName.equals(getResources().getString(R.string.cis))) {
+            TextView info = findViewById(R.id.building_info);
+            info.setText(R.string.cis_info);
+        }
+        if (BuildingName.equals(getResources().getString(R.string.csb))) {
+            TextView info = findViewById(R.id.building_info);
+            info.setText(R.string.csb_info);
+        }
     }
 }
--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.uncw.seahawktours;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +16,10 @@ public class MainActivity extends Activity {
 
     public void onClickDisplay(View view) {
         //Launch the DisplayActivity with building data
-
+        Button button = (Button)view;
+        String buttonText = button.getText().toString(); //get text from button pushed
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("building_name", buttonText);  //pass button text to new intent
+        startActivity(intent);
     }
 }
