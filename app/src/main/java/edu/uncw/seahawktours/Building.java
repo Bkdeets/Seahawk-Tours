f234aa9be83f9dbe2e2e21385ba99d8403cece29
Britton <brittonkdeets@gmail.com>
2018-10-23 13:11:06 -0400

    Added getters and setters to Building

---
+++ app/src/main/java/edu/uncw/seahawktours/Building.java
@@ -0,0 +1,23 @@
+package edu.uncw.seahawktours;
+
+public class Building {
+
+    private String name;
+    private String info;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
